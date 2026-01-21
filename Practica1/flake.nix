6c277812755e77baa02a1e7ba4ad5780c153629c
lemisora <leonardo.michel.dom@gmail.com>
2026-01-20 18:34:16 -0600

    Corrección de flake con el script para crear el environment con conda y las dependencias

--- Practica1/flake.nix
+++ Practica1/flake.nix
@@ -3,10 +3,12 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      ENV_NAME = "pda";
     in
     {
       devShells.${system}.default = pkgs.mkShell {
@@ -14,7 +16,23 @@
           conda
         ];
         shellHook = ''
-          echo "Entering the PDA Practica 1 environment"
+          # eval "$(conda shell.bash hook)"
+          # Crear el entorno si la carpeta no existe
+          ENV_DIR=$HOME/.conda/envs/${ENV_NAME};
+          if [ ! -d "$ENV_DIR" ]; then
+            echo "$ENV_DIR no existe, creando..."
+            echo "Creando entorno ${ENV_NAME} de Conda en $ENV_DIR..."
+          # Instalamos python y colorama directamente
+            conda create -n ${ENV_NAME} python=3.12 colorama -y
+          else
+            echo "Entorno ${ENV_NAME} ya existe en $ENV_DIR"
+          fi
+          
+          # "Activación" manual: exportamos el PATH de los binarios
+          # Esto funciona para Fish, Bash y Zed porque solo inyecta variables
+          export CONDA_PREFIX="$ENV_DIR"
+          export PATH="$ENV_DIR/bin:$PATH"
+          echo "Entorno '${ENV_NAME}' inyectado en el PATH"
         '';
       };
     };
