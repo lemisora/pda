59445a620fa0fb02555c7888887d68a6b4350ecf
lemisora <leonardo.michel.dom@gmail.com>
2026-01-25 14:25:17 -0600

    Configuración correcta de las dependencias para un entorno reproducible de desarrollo en C

--- Practica1-c/flake.nix
+++ Practica1-c/flake.nix
@@ -5,19 +5,31 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: 
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in
-  {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        clang
-        meson
-        ninja
-        pkg-config
-      ];
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          clang
+          clang-tools
+          meson
+          ninja
+          pkg-config
+          gdb
+          glibc.dev
+        ];
+
+        shellHook = ''
+          # Ayuda extra para que las herramientas encuentren la libc
+          export C_INCLUDE_PATH="${pkgs.glibc.dev}/include:$C_INCLUDE_PATH"
+          # export CPLUS_INCLUDE_PATH="${pkgs.glibc.dev}/include:$CPLUS_INCLUDE_PATH"
+          # export LIBRARY_PATH="${pkgs.glibc}/lib"
+          echo "Entorno de desarrollo cargado."
+        '';
+      };
     };
-  };
 }
