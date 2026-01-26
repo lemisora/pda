b7a638c3ea494e8d85b83e5a695d2ff0132a4e9f
lemisora <leonardo.michel.dom@gmail.com>
2026-01-26 09:36:08 -0600

    Se externan las funciones para procesar los comandos del cliente en el worker

--- Practica1-c/include/master.h
+++ Practica1-c/include/master.h
@@ -1,6 +1,9 @@
 #ifndef MASTER_H
 #define MASTER_H
 
+typedef struct status status_t;
+
 int master(int threshold);
+void procesar_solicitud_cliente(int socket, status_t* master_global_ptr);
 
 #endif // MASTER_H
