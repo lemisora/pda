ff6642e60c7e892374ca4ad743e37291502c90e6
lemisora <leonardo.michel.dom@gmail.com>
2026-01-26 01:27:05 -0600

    Se añade un enumerado para definir el número correspondiente a cada comando que enviarán/recibirán entre master y workers

--- Practica1-c/include/protocolo.h
+++ Practica1-c/include/protocolo.h
@@ -4,6 +4,20 @@
 #define MAXIMO 50
 #define MAX_ARCHIVOS 5
 
+typedef enum  { 
+    REG_WORKER = 0,
+    LIST_FILES = 1,
+    GET_STATUS = 2,
+    SOLICITAR_WORKER = 3,
+    CONFIRM_WORKER = 4,
+} action_t;
+
+typedef struct {
+    action_t accion;
+    char msg[128];  // Dato flexible, puede ser IP, o nombre_archivo
+    int valor;      // Valor numérico flexible
+} paquete_t;
+
 typedef struct {
     char accion[10];   
     char nombre[MAXIMO]; 
