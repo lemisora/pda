834dd2c9cc9bfb88d4837821c0fb480ddab95f8c
lemisora <leonardo.michel.dom@gmail.com>
2026-02-02 00:59:43 -0600

    Add listenPort constant

--- Prac2-Java/touche/src/main/java/com/pda/Constants/Net.java
+++ Prac2-Java/touche/src/main/java/com/pda/Constants/Net.java
@@ -3,5 +3,5 @@ package com.pda.Constants;
 public class Net {
     public static final String localhost = "127.0.0.1";
     public static final String listenIP = "0.0.0.0";
-        // final int listenPort = 8000;
+    public static final int listenPort = 8000;
 }
