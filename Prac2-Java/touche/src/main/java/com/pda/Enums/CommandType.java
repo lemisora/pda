9888f63976eb1d8426e410abfa23c68453a5b843
lemisora <leonardo.michel.dom@gmail.com>
2026-02-01 20:42:54 -0600

    Añade enum CommandType
    Con este se representan diferentes tipos de comando para operaciones de
    la clase Nodo

---
+++ Prac2-Java/touche/src/main/java/com/pda/Enums/CommandType.java
@@ -0,0 +1,6 @@
+package com.pda.Enums;
+
+public enum CommandType {
+    WRITE,  // Orden para escribir en el almacenamiento de un nodo
+    ALIVE,   // Orden para verificar si un nodo está activo
+}
