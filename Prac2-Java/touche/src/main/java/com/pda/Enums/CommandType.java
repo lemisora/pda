44cff0d709a91af78584d25d8a9416822b9aaa63
lemisora <leonardo.michel.dom@gmail.com>
2026-02-03 00:13:09 -0600

    Se añaden dos tipos de comando, 'HELLO' para iniciar elección y 'NEW_LEADER' para informar a los demás que un líder ha sido seleccionado.

--- Prac2-Java/touche/src/main/java/com/pda/Enums/CommandType.java
+++ Prac2-Java/touche/src/main/java/com/pda/Enums/CommandType.java
@@ -2,5 +2,7 @@ package com.pda.Enums;
 
 public enum CommandType {
     WRITE,  // Orden para escribir en el almacenamiento de un nodo
-    ALIVE,   // Orden para verificar si un nodo está activo
+    ALIVE,   // Orden para verificar si un nodo está activo (y como respuesta de la elección)
+    HELLO,  // Orden para votar por nuevo líder (inicia elección)
+    NEW_LEADER  // Orden para mandar el mensaje de que se es el nuevo líder
 }
