4a0ba639d66b92bfe8332d1ff6cbd22638876e76
lemisora <leonardo.michel.dom@gmail.com>
2026-02-03 01:23:08 -0600

    Se añade el comando HEARTBEAT para mandar latidos al sistema del líder a los otros nodos

--- Prac2-Java/touche/src/main/java/com/pda/Enums/CommandType.java
+++ Prac2-Java/touche/src/main/java/com/pda/Enums/CommandType.java
@@ -4,5 +4,6 @@ public enum CommandType {
     WRITE,  // Orden para escribir en el almacenamiento de un nodo
     ALIVE,   // Orden para verificar si un nodo está activo (y como respuesta de la elección)
     HELLO,  // Orden para votar por nuevo líder (inicia elección)
-    NEW_LEADER  // Orden para mandar el mensaje de que se es el nuevo líder
+    NEW_LEADER,  // Orden para mandar el mensaje de que se es el nuevo líder
+    HEARTBEAT   // Orden para enviar estado de funcionamiento del líder a los demás
 }
