a500cb20f4383645aa1406d3fd3bd19015fe782c
lemisora <leonardo.michel.dom@gmail.com>
2026-02-03 01:24:01 -0600

    Se procesan los comandos de CommandType Se añade cuerpo para cada caso

--- Prac2-Java/touche/src/main/java/com/pda/Manager/MessageManager.java
+++ Prac2-Java/touche/src/main/java/com/pda/Manager/MessageManager.java
@@ -43,5 +43,41 @@ public class MessageManager implements Runnable {
     */
     public void procesarMensaje(Mensaje mensaje) {
         System.out.println("[Nodo '" + nodo.getName() + "'] Mensaje recibido: " + mensaje.toString());
+        switch (mensaje.getCommand()) {
+            // En este caso se ha iniciado una elección
+            case HELLO -> {
+                // Si MI id es MAYOR que el del remitente, le respondo "ALIVE" (para callarlo)
+                if (nodo.getId() > mensaje.getSenderId()) {
+                    System.out.println("Recibí HELLO de " + mensaje.getSenderId() + ". Soy mayor (" + nodo.getId() + "), le respondo ALIVE.");
+
+                    Mensaje respuesta = new Mensaje(
+                            CommandType.ALIVE,
+                            nodo.getId(),
+                            nodo.getName(),
+                            nodo.getIP(),
+                            nodo.getPort(),
+                            "Detener elección, soy mayor. Nodo: " + nodo.getName() + "| id : " + nodo.getId()
+                    );
+
+                    // Respondemos directamente al host y puerto que venía en el mensaje
+                    nodo.addDataToMessageQueue(mensaje.getSenderHost(), mensaje.getSenderPort(), respuesta);
+                }
+            }
+            // Verificar si puedo ser líder y entrar en la elección
+            case ALIVE -> {
+                nodo.bullyElectionVote(mensaje.getSenderId());
+            }
+            case HEARTBEAT -> {
+                nodo.updateLastHeartbeat();
+            }
+            case NEW_LEADER -> {
+                // Hay un nuevo líder oficial. Actualizo mi estado.
+                System.out.println("Nuevo Líder reconocido: " + mensaje.getSenderName() + " (ID: " + mensaje.getSenderId() + ")");
+                nodo.setLeader(false);
+                nodo.setCandidateFailed(true); // Ya no intento ser líder
+
+                nodo.updateLastHeartbeat();
+            }
+        }
     }
 }
