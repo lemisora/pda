ce6cbec894e7821f77852d895cde4a16b0d0766a
lemisora <leonardo.michel.dom@gmail.com>
2026-02-03 00:12:08 -0600

    Se añade los campos para obtener más información del remitente y así poder devolver de forma más precisa un mensaje

--- Prac2-Java/touche/src/main/java/com/pda/Node/Mensaje.java
+++ Prac2-Java/touche/src/main/java/com/pda/Node/Mensaje.java
@@ -8,12 +8,33 @@ import java.io.Serializable;
  */
 public class Mensaje implements Serializable{
     private final CommandType command;
+    private final int senderId;
     private final String senderName;
+    private final String senderHost;
+    private final int senderPort;
     private final String data;
-    
-    public Mensaje(CommandType command, String senderName, String data) {
+
+    /**
+     * Serializador del Mensaje
+     * @param command : tipo de comando a enviar, están definidos en com.pda.Enums.CommandType
+     * @param senderId : ID del remitente del mensaje
+     * @param senderName : Nombre del remitente del mensaje
+     * @param senderHost : IP del remitente del mensaje
+     * @param senderPort : Puerto del remitente del mensaje
+     * @param data : Contenido del mensaje en forma de String
+     */
+    public Mensaje(CommandType command,
+                   int senderId,
+                   String senderName,
+                   String senderHost,
+                   int senderPort,
+                   String data
+    ) {
         this.command = command;
+        this.senderId = senderId;
         this.senderName = senderName;
+        this.senderHost = senderHost;
+        this.senderPort = senderPort;
         this.data = data;
     }
 
@@ -24,6 +45,12 @@ public class Mensaje implements Serializable{
     public String getSenderName() {
         return senderName;
     }
+
+    public String getSenderHost() { return senderHost; }
+
+    public int getSenderPort() { return senderPort; }
+
+    public int getSenderId() { return senderId; }
 
     public String getData() {
         return data;
